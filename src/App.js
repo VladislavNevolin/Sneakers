6286eaaecf65d0d356ffa28ce67c199dfea0d498
VladislavNevolin <zrebborn@gmail.com>
2023-12-02 00:16:12 +0700

    Create component CARD

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-// 54.30
+// 1.43.28
+
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="wrapper clear">
 
-     <div className="overlay">
+      <div style={{display: 'none' }}className="overlay">
         <div className="drawer">
-          <h2 className="mb-30">Shopping Cart</h2>
+          <h2 className="d-flex justify-between mb-30">Shopping Cart
+            <img className="removeBtn" src='/img/btnRemove.svg' alt='Remove' />
+          </h2>
 
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
-              <img className="mr-20" width={70} height={70} src='/img/sneakers/greenNike.jpg' alt='greenNike'/>
+              <img className="mr-20" width={70} height={70} src='/img/sneakers/greenNike.jpg' alt='greenNike' />
 
               <div className="mr-20 flex">
                 <p className="mb-5">Men sneakers Nike Blazer Mid Suede</p>
@@ -18,11 +22,11 @@ function App() {
                 <b>150 euro</b>
               </div>
 
-              <img className="removeBtn" src='/img/btnRemove.svg' alt='Remove'/>
+              <img className="removeBtn" src='/img/btnRemove.svg' alt='Remove' />
             </div>
 
             <div className="cartItem d-flex align-center mb-20">
-              <img className="mr-20" width={70} height={70} src='/img/sneakers/greenNike.jpg' alt='greenNike'/>
+              <img className="mr-20" width={70} height={70} src='/img/sneakers/greenNike.jpg' alt='greenNike' />
 
               <div className="mr-20 flex">
                 <p className="mb-5">Men sneakers Nike Blazer Mid Suede</p>
@@ -30,14 +34,33 @@ function App() {
                 <b>150 euro</b>
               </div>
 
-              <img className="removeBtn" src='/img/btnRemove.svg' alt='Remove'/>
+              <img className="removeBtn" src='/img/btnRemove.svg' alt='Remove' />
             </div>
           </div>
-        
-        
 
+          <div className="cartTotalBlock">
+            <ul >
+              <li>
+                <span>Total:</span>
+
+                <div></div>
+
+                <b>315 euro</b>
+              </li>
+
+              <li>
+                <span>Tax 5%:</span>
+
+                <div></div>
+
+                <b>15,75 euro</b>
+              </li>
+            </ul>
+
+            <button className="greenButton">Place an order<img src='/img/arrow.svg'alt='arrow' /></button>
+          </div>
         </div>
-     </div>
+      </div>
 
 
       <header className="d-flex justify-between align-center p-40">
@@ -69,89 +92,22 @@ function App() {
           <h1 className="">All sneakers</h1>
 
           <div className="searchBlock d-flex">
-            <img src="/img/searchImage.svg" alt="Search"/>
+            <img src="/img/searchImage.svg" alt="Search" />
 
             <input placeholder="Searching ..." />
           </div>
         </div>
-        
+
 
         <div className="d-flex">
-          <div className="card">
-            <div className="favorite">
-              <img src="/img/heartInactive.svg" alt="Grey Heart"/>
-            </div>
+          <Card />
 
-            <img width={133} height={112} src='/img/sneakers/greenNike.jpg' alt='greenNike' />
+          <Card />
 
-            <h5>Men sneakers Nike Blazer Mid Suede</h5>
-            
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
+          <Card />
 
-                <b>150 euro</b>
-              </div>
+          <Card />
 
-              <button className="button">
-                <img width={11} height={11} src='/img/cardSelectorPlus.svg' alt='Plus' />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src='/img/sneakers/greenNike.jpg' alt='greenNike' />
-
-            <h5>Men sneakers Nike Blazer Mid Suede</h5>
-
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
-
-                <b>150 euro</b>
-              </div>
-
-              <button className="button">
-                <img width={11} height={11} src='/img/cardSelectorPlus.svg' alt='Plus' />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src='/img/sneakers/greenNike.jpg' alt='greenNike' />
-
-            <h5>Men sneakers Nike Blazer Mid Suede</h5>
-
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
-
-                <b>150 euro</b>
-              </div>
-
-              <button className="button">
-                <img width={11} height={11} src='/img/cardSelectorPlus.svg' alt='Plus' />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src='/img/sneakers/greenNike.jpg' alt='greenNike' />
-
-            <h5>Men sneakers Nike Blazer Mid Suede</h5>
-
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
-
-                <b>150 euro</b>
-              </div>
-
-              <button className="button">
-                <img width={11} height={11} src='/img/cardSelectorPlus.svg' alt='Plus' />
-              </button>
-            </div>
-          </div>
         </div>
       </div>
     </div>
